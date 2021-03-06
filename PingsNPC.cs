71e0ce682e70905b7b4996c777b530679ef3e3f6
direwolf420 <amakejk1@web.de>
2021-03-06 15:58:05 +0100

    make NPCs despawn when outside of the world

--- PingsNPC.cs
+++ PingsNPC.cs
@@ -14,6 +14,18 @@ namespace Pings
 
 		public override bool CheckActive(NPC npc)
 		{
+			var dims = new Rectangle(16 * 40, 16 * 40, 16 * (Main.maxTilesX - 40), 16 * (Main.maxTilesY - 40));
+
+			//"Inflate" the hitbox in all directions by its dimensions for safety,
+			//Then check if its outside the "world" dimensions (excluding 40 tiles outside)
+			if (npc.Left.X - npc.width    < dims.X ||
+				npc.Right.X + npc.width   > dims.Width ||
+				npc.Top.Y - npc.height    < dims.Y ||
+				npc.Bottom.Y + npc.height > dims.Height)
+			{
+				return base.CheckActive(npc);
+			}
+
 			bool anyMatchingPing = PingsWorld.Pings.Any(p => p.PingType == PingType.NPC && p.WhoAmI == npc.whoAmI && p.Type == npc.type);
 			return !anyMatchingPing; //If there is a matching ping, return false
 		}
